ef64b6579fdbf85a9850a1328a3f6e830a35a733
Brandon McFarland <branmcf@gmail.com>
2018-02-13 15:14:47 -0600

    Genesis commit to save place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { InterestsComponent } from './components/interests/interests.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+const appRoutes: Routes = [
+  { path: 'courses', component: CoursesComponent },
+  { path: 'interests', component: InterestsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full' }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    InterestsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes), HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
